3a394fdbcb5ae11e7634ffad0599b1d1bf12c54f
Wunf <nb_wnf@126.com>
2014-11-10 15:17:34 +0800

    morph

--- ClothMotion/simulation/morph.h
+++ ClothMotion/simulation/morph.h
@@ -4,12 +4,12 @@
 #include "mesh.h"
 
 struct Morph {
-	Mesh *mesh;
-	std::vector<Mesh> targets;
-	typedef std::vector<double> Weights;
-	Spline<Weights> weights;
-	Spline<double> log_stiffness;
-	Vec3 pos (double t, const Vec2 &u) const;
+    Mesh *mesh;
+    std::vector<Mesh> targets;
+    typedef std::vector<double> Weights;
+    Spline<Weights> weights;
+    Spline<double> log_stiffness;
+    Vec3 pos (double t, const Vec3 &u) const;
 };
 
 void apply (const Morph &morph, double t);
